b95cfe40970adb6afc8562b37082317dcba6df76
D0845073 <D0845073@o365.fcu.edu.tw>
2023-05-26 16:21:19 +0800

    install and import

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
+import { BrowserRouter, HashRouter ,Route ,Routes,Switch } from 'react-router-dom';
 import './App.css';
+import './components/Appbar'
+import ButtonAppBar from './components/Appbar';
+import Login from './components/LoginPage';
+import SignUp from './components/SignUpPage';
+import UpdateAccount from './components/UpdateAccount'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter>
+      <Routes>
+        <Route path = "/" element = {<ButtonAppBar/>}/>
+        <Route path = "/login" element = {<Login/>}/>
+        <Route path = "/login/signup" element = {<SignUp/>}/>
+        <Route path = "/login/updateaccount" element = {<UpdateAccount/>}/>
+      </Routes>
+    </HashRouter>
+    
   );
 }
 
